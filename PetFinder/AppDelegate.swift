2b35f5856af03e0059419da3f06906f2112d6ebb
Justin Maronde <myemailisjustin@me.com>
2022-09-19 07:27:27 -0400

    setup reachability for app

--- PetFinder/AppDelegate.swift
+++ PetFinder/AppDelegate.swift
@@ -6,13 +6,17 @@
 //
 
 import UIKit
+import Reachability
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    let reachability = try! Reachability()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        setupReachability()
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
@@ -21,7 +25,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
+    
+    private func setupReachability() {
+        //Setup an observer for Reachability.
+        NotificationCenter.default.addObserver(self, selector: #selector(reachabilityChanged(note:)),
+                                               name: .reachabilityChanged, object: reachability)
+        
+        //Start up reachability.
+        do {
+            try reachability.startNotifier()
+        } catch {
+            print("Unable to start notifier")
+        }
+    }
 
+    @objc func reachabilityChanged(note: Notification) {
+      let reachability = note.object as! Reachability
+
+      switch reachability.connection {
+          case .wifi:
+            print("Reachable via WiFi")
+          case .cellular:
+            print("Reachable via Cellular")
+          case .unavailable:
+            print("Network not reachable")
+          case .none:
+            print("Network not reachable")
+      }
+    }
 
 }
 

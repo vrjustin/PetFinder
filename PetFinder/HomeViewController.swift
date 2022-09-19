3199cce83f152b129a283edff7406cb5a55e38d8
Justin Maronde <myemailisjustin@me.com>
2022-09-19 07:58:42 -0400

    Test to verify we are NOT checking in our client_id or client_secret to scm.

--- PetFinder/HomeViewController.swift
+++ PetFinder/HomeViewController.swift
@@ -12,6 +12,13 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.systemPink
+        
+        //Test if we can access our API Secrets now.
+        if let client_id = Bundle.main.infoDictionary?["CLIENT_ID"] as? String, let client_secret = Bundle.main.infoDictionary?["CLIENT_SECRET"] as? String  {
+            print("Ok clientID is: \(client_id)")
+            print("Ok clientSecret is: \(client_secret)")
+        }
+        
     }
 
 

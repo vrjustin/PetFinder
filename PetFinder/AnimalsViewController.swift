fb6cd849a404495cceebc20e678736e80eeacb27
VRJustin <myemailisjustin@me.com>
2024-02-19 13:26:26 -0500

    Feature/new-ui-testing-bundle-baseline (#20)
    * add in searchContainerView so we can start building out the search parameter inputs.
    
    * present a FilterViewController when the filter button is pressed.
    
    * add observer pattern to the AVC so we know when the distance slider changes value.
    
    * return the thumbnail for display of the animal
    
    * add ui testing target bundle, etc

--- PetFinder/AnimalsViewController.swift
+++ PetFinder/AnimalsViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 
 private let animalCellReuseIdentifier = "animalCellReuseIdentifier"
 
-class AnimalsViewController: UITableViewController {
+class AnimalsViewController: UIViewController {
     
     // MARK: - PROPERTIES
+    static let filterValueChanged = Notification.Name("filterValueChanged")
     var viewModel: AnimalsViewModel!
+    
+    private lazy var tableView: UITableView = {
+        let tv = UITableView()
+        tv.backgroundColor = .gray
+        tv.dataSource = self
+        return tv
+    }()
     
     private var animals = [Animal]() {
         didSet {
@@ -43,6 +51,11 @@ class AnimalsViewController: UITableViewController {
         super.viewDidLoad()
         setupUI()
         fetchAnimals()
+        setupObservers()
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
     
     // MARK: - API
@@ -53,7 +66,7 @@ class AnimalsViewController: UITableViewController {
         
         //TODO: Remove hard-coded location & distance from location. These should come from the users's device location if allowed or
         //they should input the zip they want to search. And a Search distance.
-        let requestModel = FetchAnimalRequestModel(animalType: viewModel.currentAnimalType, breed: viewModel.currentAnimalBreed, location: "90210", distance: "25")
+        let requestModel = FetchAnimalRequestModel(animalType: viewModel.currentAnimalType, breed: viewModel.currentAnimalBreed, location: "48334", distance: "25")
                 
         apiManager.fetchAnimalForSelectedBreed(requestModel: requestModel) { animals in
             if let animals = animals {
@@ -69,30 +82,64 @@ class AnimalsViewController: UITableViewController {
     private func setupUI() {
         view.backgroundColor = .yellow
         navigationItem.title = "Results of Animals"
+        configureNavbar()
         configureTableView()
     }
     
+    private func configureNavbar() {
+        print("OK lets add a filter icon to the top right of the navbar..")
+        let filterButton = UIBarButtonItem(image: UIImage(systemName: "line.3.horizontal.decrease"), style: .plain, target: self, action: #selector(filterButtonTapped))
+
+        navigationItem.rightBarButtonItem = filterButton
+    }
+    
     private func configureTableView() {
+        self.view.addSubview(tableView)
         tableView.register(AnimalCell.self, forCellReuseIdentifier: animalCellReuseIdentifier)
         tableView.rowHeight = 100
         tableView.separatorStyle = .none
+        
+        tableView.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor)
     }
+    
+    private func setupObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleFilterValueChanged(_:)), name: AnimalsViewController.filterValueChanged, object: nil)
+
+    }
+    
+    // MARK: - ACTIONS
+
+    @objc func filterButtonTapped() {
+        print("Filter Button Tapped")
+        let filterVC = FilterViewController()
+        filterVC.modalPresentationStyle = .formSheet
+        filterVC.modalTransitionStyle = .coverVertical
+        present(filterVC, animated: true)
+    }
+    
+    @objc func handleFilterValueChanged(_ notification: Notification) {
+        if let value = notification.object as? Float {
+            // Update your AnimalsViewController with the new value
+            print("AnimalsVC received value: \(value)")
+        }
+    }
+
     
 }
 
-extension AnimalsViewController {
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+extension AnimalsViewController: UITableViewDataSource, UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return animals.count
     }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: animalCellReuseIdentifier, for: indexPath) as! AnimalCell
         let animal = animals[indexPath.row]
-        
+
         cell.viewModel = AnimalViewModel(withAnimal: animal)
-        
+
         return cell
     }
-    
+
 }
